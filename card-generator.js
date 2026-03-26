36793d230ebab2072a5f8c19396f5b909a87d565
JEM <jem@ik.me>
2026-03-26 07:13:27 +0100

    Add files via upload

--- card-generator.js
+++ card-generator.js
@@ -186,6 +186,16 @@ function drawBackCard(doc, track, x, y, w, h, settings) {
   const bgImg = generateBackBg(w, h, scheme.backBg, decadeColor);
   doc.addImage(bgImg, 'PNG', x, y, w, h);
 
+  // Vinyl rings
+  doc.setDrawColor(255, 255, 255);
+  doc.setLineWidth(0.25);
+  doc.setGState && doc.setGState(new doc.GState({ opacity: 0.1 }));
+  const vcx = x + w / 2, vcy = y + h * 0.52;
+  for (let r = 3; r <= 18; r += 3) doc.circle(vcx, vcy, r, 'S');
+  doc.setFillColor(255, 255, 255);
+  doc.circle(vcx, vcy, 1.5, 'F');
+  doc.setGState && doc.setGState(new doc.GState({ opacity: 1 }));
+
   // Decade color bar (top)
   if (settings.decadeBar) {
     const dRgb = hexToRgb(decadeColor);
